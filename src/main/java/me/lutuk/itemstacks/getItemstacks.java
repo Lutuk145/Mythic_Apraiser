33e2b2de9005eccfee666e6d490c53fb26134b80
Lutuk145 <lukas.knej@gmail.com>
2023-10-12 19:33:22 +0200

    Removed debug and added colors

--- src/main/java/me/lutuk/itemstacks/getItemstacks.java
+++ src/main/java/me/lutuk/itemstacks/getItemstacks.java
@@ -6,17 +6,12 @@ import net.minecraft.item.ItemStack;
 
 import java.io.IOException;
 
-import static me.lutuk.ExampleMod.debugMode;
 
 public class getItemstacks {
     public static void tick() throws IOException {
-        if (debugMode) {
-            getInventoryItemDebug();
-            getContainerItemDebug();
-        } else {
-            getInventoryItem();
-            getContainerItem();
-        }
+
+        getInventoryItem();
+        getContainerItem();
     }
 
     public static void getInventoryItem() throws IOException {
@@ -24,9 +19,9 @@ public class getItemstacks {
             return;
         }
         for (int i = 0; i < MinecraftClient.getInstance().player.getInventory().size(); i++) {
-            if (MinecraftClient.getInstance().player.getInventory().getStack(i).isEmpty()){
+            if (MinecraftClient.getInstance().player.getInventory().getStack(i).isEmpty()) {
                 MinecraftClient.getInstance().player.getInventory().setStack(i, ItemStack.EMPTY);
-            }else {
+            } else {
                 ItemStack currentItem = MinecraftClient.getInstance().player.getInventory().getStack(i);
                 CodingUtils.mythicCheck(currentItem);
                 MinecraftClient.getInstance().player.getInventory().updateItems();
@@ -44,7 +39,7 @@ public class getItemstacks {
         }
         for (int i = 0; i < MinecraftClient.getInstance().player.currentScreenHandler.slots.size(); i++) {
             ItemStack stack = MinecraftClient.getInstance().player.currentScreenHandler.slots.get(i).getStack();
-            if (stack.isEmpty()){
+            if (stack.isEmpty()) {
                 MinecraftClient.getInstance().player.currentScreenHandler.slots.get(i).setStack(ItemStack.EMPTY);
             }
             stack = CodingUtils.mythicCheck(stack);
@@ -81,9 +76,9 @@ public class getItemstacks {
             return;
         }
         for (int i = 0; i < MinecraftClient.getInstance().player.getInventory().size(); i++) {
-            if (MinecraftClient.getInstance().player.getInventory().getStack(i).isEmpty()){
+            if (MinecraftClient.getInstance().player.getInventory().getStack(i).isEmpty()) {
                 MinecraftClient.getInstance().player.getInventory().setStack(i, ItemStack.EMPTY);
-            }else {
+            } else {
                 ItemStack currentItem = MinecraftClient.getInstance().player.getInventory().getStack(i);
                 CodingUtils.mythicCheck(currentItem);
                 MinecraftClient.getInstance().player.getInventory().updateItems();
